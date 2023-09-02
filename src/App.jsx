5941d9a912deea0257fb28722096078d8f83d765
Farhan Ahmed <farhanf7n@gmail.com>
2023-09-02 05:44:28 +0400

    Finished the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
 
 function App() {
-    const [images, setImages] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [term, SetTerm] = useState('')
-    return (
-    <>
-        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-            <img src="https://source.unsplash.com/random" alt="" className='w-full'/>
-            <div className="px-6 py-4 ">
-            <div className="font-bold text-purple-500 text-xl mb-2">
-                Photo by John Doe
-            </div>
-            <ul>
-                <li>
-                    <strong>Views: 300</strong>
-                </li>
-                <li>
-                    <strong>Downloads: 2400</strong>
-                </li>
-                <li>
-                    <strong>Likes: 7000</strong>
-                </li>
-            </ul>
-        </div>
-        <div className="px-6 py-4">
-            <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #Tag1
-            </span>
-            <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #Tag2
-            </span>
-            <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                #Tag3
-            </span>
-        </div>
-        </div>
-        
-    </>
-  )
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=38928262-472f1bbf3661688c5245fc3c3&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
+  );
 }
 
-export default App
+export default App;
